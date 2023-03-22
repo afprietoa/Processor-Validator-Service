dbf6b7da27e419c585a8f892edacd872544a1388
Andrés Prieto <68924563+afprietoa@users.noreply.github.com>
2023-03-22 08:44:07 -0500

    Update CsvController.java

--- processor/src/main/java/com/makaia/processor/controllers/CsvController.java
+++ processor/src/main/java/com/makaia/processor/controllers/CsvController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@RestController
+@RequestMapping("/api/v1")
 public class CsvController {
     private CsvService csvService;
 
